a8462f4c38c8ea703abc26fcf59fb84f69228696
wagonghao <vaststarsea@foxmail.com>
2019-12-16 09:35:08 +0800

    upgrade vue version

--- web/fisco-bcos-browser-front/config/index.js
+++ web/fisco-bcos-browser-front/config/index.js
@@ -12,7 +12,7 @@ module.exports = {
         assetsPublicPath: '/',
         proxyTable: {
             '/api': {
-                target: 'http://127.0.0.1:5100',
+                target: 'http://127.0.0.1',
                 changeOrigin: true,
                 pathRewrite: {
                     '^/api': ''
